72dcd145d465e1e90ce65587b071159958abb14b
Neil <neil.mcalpine@musicqubed.com>
2016-06-07 22:22:47 +0300

    update

--- devserver.bat
+++ devserver.bat
@@ -1,1 +1,1 @@
-python "C:\Program Files\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py" app.yaml
+python "C:\Program Files (x86)\Google\Cloud SDK\google-cloud-sdk\bin\dev_appserver.py" app.yaml
